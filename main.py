eee215b2654e3ea2ef679817ceff52d55b1b2bad
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2023-12-18 03:32:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -450,10 +450,12 @@ class ManifestAutoUpdate:
             app = fresh_resp['apps'][app_id]
             manifests = cdn.get_manifests(int(app_id),app)
             with lock:
+                if manifests['depots']:
+                    if int(app_id) not in self.user_info[username]['app']:
+                    self.user_info[username]['app'].append(int(app_id))
                 if not manifests['manifests']:
                     continue
-                if int(app_id) not in self.user_info[username]['app']:
-                    self.user_info[username]['app'].append(int(app_id))
+                
             #尝试获取dlc或额外内容并添加到配置文件(仅添加拥有的DLC)
             package = {'dlcs': [], 'packagedlcs': []}
             dlcappids = {}
