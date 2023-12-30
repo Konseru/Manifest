2386e03912e9a2213d3d2da0033b91b87e588be4
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2023-12-31 02:16:03 +0800

    Update push.py

--- push.py
+++ push.py
@@ -48,10 +48,10 @@ def push(repo=None,delete_tag=set()):
                     result_list.append(
                         pool.map_async(subprocess.check_call, (['git', 'push', 'origin', local_head.name],)))
         #删除旧的标签            
-        for tag in remote_tag_list:
+        for remote_sha, remote_tag in remote_tag_list:
             with lock:
-                print('delete_tag => ', tag)
-            result_list.append(pool.map_async(subprocess.check_call, (['git', 'push', 'origin', f':refs/tags/{tag}'],)))
+                print('delete_tag => ', remote_tag)
+            result_list.append(pool.map_async(subprocess.check_call, (['git', 'push', 'origin', f':refs/tags/{remote_tag}'],)))
         for local_tag in repo.tags:
             for remote_sha, remote_tag in remote_tag_list:
                 if remote_tag == local_tag.name:
