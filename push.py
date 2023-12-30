1ca9d99c7de9f431ee35de76cd835085bdab9e3c
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2023-12-31 02:27:03 +0800

    Update push.py

--- push.py
+++ push.py
@@ -32,8 +32,9 @@ def push(repo=None,delete_tag=set()):
     total_branch = 0
     total_tag = 0
     for remote_sha, remote_tag in remote_tag_list:
-        print('delete_tag => ', remote_tag)
-        subprocess.check_call(['git', 'push', 'origin', f':refs/tags/{remote_tag}'])
+        if remote_tag != 'Steam_助手':
+            print('delete_tag => ', remote_tag)
+            subprocess.check_call(['git', 'push', 'origin', f':refs/tags/{remote_tag}'])
     with Pool(8) as pool:
         pool: ThreadPool
         result_list = []
