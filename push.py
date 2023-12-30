52318aa5474ef0bbec7f52c851e887f5608e6ec8
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2023-12-31 04:35:53 +0800

    Update push.py

--- push.py
+++ push.py
@@ -34,10 +34,7 @@ def push(repo=None,delete_tag=set()):
     
     with Pool(8) as pool:
         pool: ThreadPool
-        result_list = []
-        for remote_sha, remote_head in remote_head_list:
-            if not remote_head in ["main", "app", "data"]:
-               result_list.append(pool.map_async(subprocess.check_call, (['git', 'push', 'origin', f':{remote_head}'],)))     
+        result_list = []  
         for local_head in repo.heads:
             if local_head.name.isdecimal():
                 for remote_sha, remote_head in remote_head_list:
