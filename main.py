2e4a51c9dad6dffa66de8b3dbda4eaa67d2fc34d
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2024-01-01 02:52:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -304,7 +304,7 @@ class ManifestAutoUpdate:
         if str(app_id) not in self.get_app_worktree():
             if app_path.exists():
                 app_path.unlink(missing_ok=True)
-            if not self.check_app_repo_remote(app_id):
+            if self.check_app_repo_remote(app_id):
                 with lock:
                     if not self.check_app_repo_local(app_id):
                         self.repo.git.fetch('origin', f'{app_id}:origin_{app_id}')
@@ -382,7 +382,7 @@ class ManifestAutoUpdate:
     def async_task(self, cdn, app_id,appinfo,package,depot):
         self.init_app_repo(app_id)
         manifest_path = self.ROOT / f'depots/{app_id}/{depot.depot_id}_{depot.gid}.manifest'
-        if manifest_path.exists():
+        if not manifest_path.exists():
             self.log.debug(f'manifest_path exists: {manifest_path}')
             app_repo = git.Repo(self.ROOT / f'depots/{app_id}')
             try:
