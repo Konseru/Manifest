b8a19299c35bd0fccb204d637e676e0798dddedf
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2024-01-02 07:32:03 +0800

    Update main.py

--- DepotManifestGen/main.py
+++ DepotManifestGen/main.py
@@ -269,6 +269,7 @@ class MyCDNClient(CDNClient):
         for package_id, info in iteritems(self.steam.get_product_info(packages=packages,timeout=30)['packages']):
             if 'depotids' in info and info['depotids'] and info['billingtype'] in BillingType.PaidList:
                 app_id_list.extend(list(info['appids'].values()))
+                log.info(f'{info}')
             self.licensed_app_ids.update(info['appids'].values())
             self.licensed_depot_ids.update(info['depotids'].values())
         return app_id_list
