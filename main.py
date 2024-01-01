7898f5d765994e866726028e7db16e7a5d32e5e4
BlankTMing <151583193+BlankTMing@users.noreply.github.com>
2024-01-02 07:42:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -447,9 +447,10 @@ class ManifestAutoUpdate:
             return
         job_list = []
         flag = True    
-        app_token = steam.get_access_tokens(app_id_list)
-        self.log.info(f'{app_token}')
+        app_token = steam.get_access_tokens(app_id_list)['apps']
         for app_id in app_id_list:
+           
+            self.log.info(f'{app_token[app_id]}')
             if self.update_app_id_list and int(app_id) not in self.update_app_id_list:
                 continue
             app = fresh_resp['apps'][app_id]
